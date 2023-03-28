88fa81c4c68218f4abaad7052d60b5ba21c09651
rodrigo <rodrigogranemel@gmail.com>
2023-03-28 11:17:34 -0300

    img fix

---
+++ js/textbox-cfg.js
@@ -0,0 +1,6 @@
+var typed = new Typed("#auto-type2", {
+    strings: ["Part Picker", "Part Selector", "Part Finder"],
+    typeSpeed: 160,
+    backSpeed: 160,
+    loop: true
+});
---
+++ js/textbox2-cfg.js
@@ -0,0 +1,6 @@
+var typed = new Typed("#auto-type3", {
+    strings: ["Price Comparison", "Price Search", "Price Chart"],
+    typeSpeed: 160,
+    backSpeed: 160,
+    loop: true
+});
